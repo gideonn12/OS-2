bf7b9486e6412d96f76bb683ec647fd732bee5d2
gideonn12 <gideoneeman@icloud.com>
2024-06-20 11:23:49 +0300

    init p2

--- part1.c
+++ part1.c
@@ -1,44 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>	
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 // 329924567 Gideon Neeman
-int main(int argc, char *argv[]) {
-    if (argc != 5) {
-    fprintf(stderr, "Usage: %s <parent_message> <child1_message> <child2_message> <count>", argv[0]);
-    return 1;
+int main(int argc, char *argv[])
+{
+    if (argc != 5)
+    {
+        fprintf(stderr, "Usage: %s <parent_message> <child1_message> <child2_message> <count>", argv[0]);
+        return 1;
     }
     FILE *f;
     f = fopen("output.txt", "w");
     int times = atoi(argv[4]);
     __pid_t pid = fork();
-    if(pid < 0) {
+    if (pid < 0)
+    {
         perror("fork");
         exit(1);
     }
-    if(pid == 0){
-        for(int i = 0; i < times; i++){
+    if (pid == 0)
+    {
+        for (int i = 0; i < times; i++)
+        {
             fprintf(f, "%s\n", argv[2]);
         }
         exit(0);
     }
-    else{
+    else
+    {
         __pid_t pid2 = fork();
-        if(pid2 < 0){
+        if (pid2 < 0)
+        {
             perror("fork");
             exit(1);
         }
-        if (pid2 == 0){
-            for(int i = 0; i < times; i++){
+        if (pid2 == 0)
+        {
+            for (int i = 0; i < times; i++)
+            {
                 fprintf(f, "%s\n", argv[3]);
             }
             exit(0);
         }
-        else{
+        else
+        {
             wait(NULL);
             wait(NULL);
-            for(int i = 0; i < times; i++){
+            for (int i = 0; i < times; i++)
+            {
                 fprintf(f, "%s\n", argv[1]);
             }
             fclose(f);
---
+++ part2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+// 329924567 Gideon Neeman
+
+int main(int argc, char* argv[]){
+    return 0;
+}
