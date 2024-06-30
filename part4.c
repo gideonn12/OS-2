8b833ea1c8477c9aa92c6aff155180c8b6713499
gideonn12 <gideoneeman@icloud.com>
2024-06-30 22:39:45 +0300

    added check

--- part4.c
+++ part4.c
@@ -3,29 +3,50 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-void print_usage(const char *prog_name) {
+void print_usage(const char *prog_name)
+{
     fprintf(stderr, "Usage: %s [-l] [-p] <source_directory> <destination_directory>", prog_name);
     fprintf(stderr, "  -l: Copy symbolic links as links");
     fprintf(stderr, "  -p: Copy file permissions");
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int opt = 1;
     int copy_symlinks = 0;
     int copy_permissions = 0;
-    if(argc < 3 ) {
+    if (argc < 3 || argc > 5)
+    {
         print_usage(argv[0]);
         return 1;
     }
-    if(argc == 3 && (strcmp(argv[1], "-l") == 0 || strcmp(argv[1], "-p") == 0)) {
+    if(argc == 3 && (((strcmp(argv[1], "-l") == 0 || strcmp(argv[1], "-p") == 0))))
+    {
         print_usage(argv[0]);
         return 1;
     }
-    if(strcmp(argv[1], "-l") == 0 || strcmp(argv[2], "-l") == 0){
+    if(argc == 3 && (strcmp(argv[1], "-l") == 0 && strcmp(argv[2], "-p") == 0))
+    {
+        print_usage(argv[0]);
+        return 1;
+    }
+    if(argc ==4 && (strcmp(argv[1], "-l") == 0 && strcmp(argv[2], "-p") == 0))
+    {
+        print_usage(argv[0]);
+        return 1;
+    }
+    if (argc == 5 && ((strcmp(argv[1], "-l") != 0) || (strcmp(argv[2], "-p") != 0)))
+    {
+        print_usage(argv[0]);
+        return 1;
+    }
+    if (strcmp(argv[1], "-l") == 0 || strcmp(argv[2], "-l") == 0)
+    {
         copy_symlinks = 1;
         opt++;
     }
-    if(strcmp(argv[1], "-p") == 0 || strcmp(argv[2], "-p") == 0){
+    if (strcmp(argv[1], "-p") == 0 || strcmp(argv[2], "-p") == 0)
+    {
         copy_permissions = 1;
         opt++;
     }
