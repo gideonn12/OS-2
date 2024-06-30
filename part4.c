8e7e93d2fdab7907be5e5c19aff4d11ed612ed88
gideonn12 <gideoneeman@icloud.com>
2024-06-30 22:19:55 +0300

    error checking

--- part4.c
+++ part4.c
@@ -13,7 +13,14 @@ int main(int argc, char *argv[]) {
     int opt = 1;
     int copy_symlinks = 0;
     int copy_permissions = 0;
-
+    if(argc < 3 ) {
+        print_usage(argv[0]);
+        return 1;
+    }
+    if(argc == 3 && (strcmp(argv[1], "-l") == 0 || strcmp(argv[1], "-p") == 0)) {
+        print_usage(argv[0]);
+        return 1;
+    }
     if(strcmp(argv[1], "-l") == 0 || strcmp(argv[2], "-l") == 0){
         copy_symlinks = 1;
         opt++;
